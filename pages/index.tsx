7963105474c38ff5de0af4ab8606ac72ddaacade
suito.y <suito.y@gmail.com>
2024-04-04 23:23:07 +0900

    initial

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,58 @@
-import Link from "next/link";
-import Layout from "../components/Layout";
+import { GetServerSideProps, NextPage } from "next";
+import { useState } from "react";
+import styles from "./index.module.css";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-);
+type Props = {
+    initialImageUrl: string
+}
 
-export default IndexPage;
+const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
+    const [imageUrl, setImageUrl] = useState(initialImageUrl)
+    const [loading, setLoading] = useState(false)
+    // useEffect(() => {
+    //     (async () => {
+    //         const image = await fetchImage()
+    //         setImageUrl(image.url)
+    //         setLoading(false)
+    //     })()
+    // }, [])
+
+    const handleClick = async () => {
+        setLoading(true)
+        const newImage = await fetchImage()
+        setImageUrl(newImage.url)
+        setLoading(false)
+    }
+
+    return (
+        <div className={styles.page}>
+            <button onClick={handleClick} className={styles.button}>他の猫も見る</button>
+            <div className={styles.frame}>{loading ? "読み込み中" : <img src={imageUrl} />}</div>
+        </div>
+    )
+
+}
+export default IndexPage
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+    const image = await fetchImage()
+    return {
+        props: {
+            initialImageUrl: image.url
+        }
+    }
+}
+
+type Image = {
+    url: string
+}
+const fetchImage = async (): Promise<Image> => {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search")
+    const images = await res.json()
+    console.log(images)
+    return images[0]
+}
+
+fetchImage().then((image) => {
+    console.log(image.url)
+})
